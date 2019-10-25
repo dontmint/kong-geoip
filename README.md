43567b9e0d0660604f2b035be7ca96deae91fabe
dontmint <nguyenductin@hotmail.com.vn>
2019-10-25 13:37:58 +0700

    Update deny access logic

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 ## INSTALL
 
-```bash
-geoip-devel >= 1.6.5
-https://centos.pkgs.org/7/atomic-x86_64/GeoIP-devel-1.6.9-2.el7.art.x86_64.rpm.html
+
+
+* Require geoip-devel >= 1.6.5
+* For CentOS 7, you can use these packages form Atomic Repo
+Download latest atomic-release rpm from
+http://www6.atomicorp.com/channels/atomic/centos/7/x86_64/RPMS/
+Install atomic-release rpm:
+# rpm -Uvh atomic-release*rpm
+Install GeoIP-devel rpm package:
+# yum install GeoIP-devel
 
 https://github.com/leafo/luajit-geoip
-```
 
 ## CONFIG WITH KONG
 
--- handler.lua
+++ handler.lua
@@ -1,47 +1,53 @@
 local plugin_name = ({...})[1]:match("^kong%.plugins%.([^%.]+)")
-
 local plugin = require("kong.plugins.base_plugin"):extend()
---
 local geoip_module = require 'geoip'
+
 gi = geoip_module.GeoIP()
 gi:load_databases("memory")
 
 function plugin:new()
-  plugin.super.new(self, plugin_name)
+  	plugin.super.new(self, plugin_name)
 end
 
 -- Header_filter Phase
 function plugin:header_filter(conf)
-  plugin.super.access(self)
-  if not conf then
-  kong.response.exit(500, "Cannot read plugin config")
-  end
+  	plugin.super.access(self)
+	if not conf then
+  		kong.response.exit(500, "Cannot read plugin config")
+  	end
 end
 
 -- Access Phase
 function plugin:access(conf)
-  plugin.super.access(self)
-  local country_code = gi:lookup_addr(ngx.var.remote_addr).country_code
---[DEBUG] kong.log.err("Plugin config type : ", country_code)
-  block = 0
-  if ( conf.mode == "Blacklist" and conf.blacklist_countries ~= nil ) then 
-    for i,line in ipairs(conf.blacklist_countries) do
-      if line == country_code then
-        block = 1
-      end
-    end
-  elseif ( conf.mode == "Whitelist" and conf.whitelist_countries ~= nil ) then
-    block = 1
-    for i,line in ipairs(conf.whitelist_countries) do
-      if line == country_code then
-        block = 0
-      end
-    end
-  end
-  
-  if block == 1 then 
-    kong.response.exit(403, "Access Forbiden")
-  end
+	plugin.super.access(self)
+	local country = gi:lookup_addr(ngx.var.remote_addr)
+	if not country then
+		kong.log.err("Plugin DEBUG message : Country not found : ", ngx.var.remote_addr)
+		return
+  	end 
+
+  	local block = 0
+  	if ( conf.mode == "Blacklist" and conf.blacklist_countries ~= nil ) then 
+		local country_code = country["country_code"]
+  		for i,line in ipairs(conf.blacklist_countries) do
+  			if line == country_code then
+        			block = 1
+      			end
+    		end
+  	elseif ( conf.mode == "Whitelist" and conf.whitelist_countries ~= nil ) then
+    		block = 1
+		local country_code = country["country_code"]
+    		for i,line in ipairs(conf.whitelist_countries) do
+      			if line == country_code then
+        			block = 0
+      			end
+    		end
+	elseif ( conf.mode == "Whitelist" and conf.whitelist_countries == nil ) then
+		block = 1
+  	end
+  	if block == 1 then
+  		kong.response.exit(403, "Access not available in your country : " .. country["country_name"])
+  	end
 end
 
 plugin.PRIORITY = 991
