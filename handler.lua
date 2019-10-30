79e6c80067e30d69f9a92aeba325a3eee97b74fc
dontmint <nguyenductin@hotmail.com.vn>
2019-10-30 09:34:58 +0700

    Update spacing and order

--- handler.lua
+++ handler.lua
@@ -6,83 +6,84 @@ gi = geoip_module.GeoIP()
 gi:load_databases("memory")
 
 function plugin:new()
-  	plugin.super.new(self, plugin_name)
+  plugin.super.new(self, plugin_name)
 end
 
 -- Header_filter Phase
 function plugin:header_filter(conf)
-  	plugin.super.access(self)
-	if not conf then
-  		kong.response.exit(500, "Cannot read plugin config")
-  	end
+  plugin.super.access(self)
+    if not conf then
+      kong.response.exit(500, "Cannot read plugin config")
+    end
 end
 
 -- Access Phase
 function plugin:access(conf)
-	plugin.super.access(self)
-	local country = gi:lookup_addr(ngx.var.remote_addr)
+  plugin.super.access(self)
+  local country = gi:lookup_addr(ngx.var.remote_addr)
 	if not country then
-		kong.log.err("Plugin DEBUG message : Country not found : ", ngx.var.remote_addr)
-		kong.service.request.set_header(conf.inject_country_header, "VN")
-		return
-  	end
+    kong.log.err("Plugin DEBUG message : Country not found : ", ngx.var.remote_addr)
+    kong.service.request.set_header(conf.inject_country_header, "VN")
+    return
+  end
 
 	-- INJECT HEADER 
-	if conf.inject_country_header ~= nil then 
-		kong.service.request.set_header(conf.inject_country_header, country["country_code"])
+	if conf.inject_country_header ~= nil then
+    kong.service.request.set_header(conf.inject_country_header, country["country_code"])
 	end
 	
 	if conf.inject_isp_header ~= nil then
-                kong.service.request.set_header(conf.inject_isp_header, mapISP(country["asnum"]) )
-        end
+    kong.service.request.set_header(conf.inject_isp_header, mapISP(country["asnum"]) )
+  end
 	-- BLOCK IP IF MATCH RULES
-  	local block = 0
-  	if ( conf.mode == "Blacklist" and conf.blacklist_countries ~= nil ) then 
-		local country_code = country["country_code"]
-  		for i,line in ipairs(conf.blacklist_countries) do
-  			if line == country_code then
-        			block = 1
-      			end
-    		end
-  	elseif ( conf.mode == "Whitelist" and conf.whitelist_countries ~= nil ) then
-    		block = 1
-		local country_code = country["country_code"]
-    		for i,line in ipairs(conf.whitelist_countries) do
-      			if line == country_code then
-        			block = 0
-      			end
-    		end
+  local block = 0
+  if ( conf.mode == "Blacklist" and conf.blacklist_countries ~= nil ) then 
+    local country_code = country["country_code"]
+    for i,line in ipairs(conf.blacklist_countries) do
+      if line == country_code then
+        block = 1
+      end 
+    end 
+  elseif ( conf.mode == "Whitelist" and conf.whitelist_countries ~= nil ) then
+    block = 1
+    local country_code = country["country_code"]
+    for i,line in ipairs(conf.whitelist_countries) do
+      if line == country_code then
+        block = 0
+      end 	
+    end
 	elseif ( conf.mode == "Whitelist" and conf.whitelist_countries == nil ) then
-		block = 1
-  	end
-  	if block == 1 then
-  		kong.response.exit(403, "Access not available in your country : " .. country["country_name"])
-  	end
+    block = 1
+  end
+  	
+  if block == 1 then
+    kong.response.exit(403, "Access not available in your country : " .. country["country_name"])
+  end
 end
 
 function mapISP(asnum)
 -- https://ipinfo.io/countries/vn
-isp = {   
-      	["FPT"] = { "AS18403", "AS45894", "AS131402" },
-        ["VNPT"] = {"AS45899","AS7643","AS135939","AS135965","AS45898"},
-	["VIETTEL"] = { "AS7552", "AS24086" },
+  isp = {
+  ["FPT"] = { "AS18403", "AS45894", "AS131402" },
+  ["VNPT"] = {"AS45899","AS7643","AS135939","AS135965","AS45898"},
+  ["VIETTEL"] = { "AS7552", "AS24086" },
 	["SPT"] = { "AS7602" },
-      }
+}
 
-	for isp_name,table in pairs(isp) do
-		if contains(table, asnum) then
-			return isp_name	
-		end		
-	end
-	return "other"
-	
+for isp_name,table in pairs(isp) do 
+  if contains(table, asnum) then
+    return isp_name
+  end	
+end
+-- IF ISP NOT FOUND, RETURN DEFAULT VALUE
+return "other"
 end
 
 function contains(list, asn_string)
-        for _,v in ipairs(list) do
-                if string.match(asn_string, v) then return true end
-        end
-        return false
+  for _,v in ipairs(list) do
+    if string.match(asn_string, v) then return true end 
+  end
+return false
 end
 
 plugin.PRIORITY = 1994
--- schema.lua
+++ schema.lua
@@ -4,20 +4,21 @@ local typedefs = require "kong.db.schema.typedefs"
 return {
     name = "geoip",
     fields = {
-        { config = {
-            type = "record",
-            fields = { 
-              { inject_country_header = { type = "string" }, },
-	      { inject_isp_header = { type = "string" }, },
-              { whitelist_countries = { type = "array", elements = {type = "string" } }, },
-              { blacklist_countries = { type = "array", elements = {type = "string" } }, },
-              { mode = {
-                  type = "string",
-                  default = "Blacklist",
-                  one_of = { "Whitelist", "Blacklist" },
-              }, },
-            },
+      { config = {
+        type = "record",
+        fields = { 
+          { inject_country_header = { type = "string" }, },
+          { inject_isp_header = { type = "string" }, },
+          { whitelist_countries = { type = "array", elements = {type = "string" } }, },
+          { blacklist_countries = { type = "array", elements = {type = "string" } }, },
+          { mode = {
+            type = "string",
+            default = "Blacklist",
+            one_of = { "Whitelist", "Blacklist" },
+            }, 
           },
         },
-    }
+      },
+    },
+  },
 }
